124d97a8d9fb9a586fa142e59ca472548733b1d0
hyeonjy <86361624+hyeonjy@users.noreply.github.com>
2024-10-02 20:07:28 +0900

    scroll 문제

--- js/scroll.js
+++ js/scroll.js
@@ -1,19 +1,19 @@
 $(".scroll_move1").click(function (event) {
   event.preventDefault();
-  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 100);
 });
 
 $(".scroll_move2").click(function (event) {
   event.preventDefault();
-  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 700);
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 100);
 });
 
 $(".scroll_move3").click(function (event) {
   event.preventDefault();
-  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 1100);
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 100);
 });
 
 $(".scroll_move4").click(function (event) {
   event.preventDefault();
-  $("html,body").animate({ scrollTop: $(this.hash).offset().top },1100);
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 100);
 });
