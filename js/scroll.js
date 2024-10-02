cc20ec38bbc4cafb4708cad606178186ac7ad258
Choi-kanggun <kg4889@gmail.com>
2024-10-02 19:33:41 +0900

    feat:스크롤 수정 완료

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,5 @@
-$(document).ready(function ($) {
-  $("#scroll_move").click(function (event) {
-    console.log("#scroll_move");
-    event.preventDefault();
-    $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
-  });
+$(".scroll_move").click(function (event) {
+  event.preventDefault();
+  $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 1000);
 });
+
